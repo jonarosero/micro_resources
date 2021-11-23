f144bcfb3e32dcb8027da022f9a5a1cea32d62ca
jonarosero <jars9605@hotmail.com>
2021-11-22 22:27:11 -0500

    Fix: pedidos models

--- models/pedido_models/pedido.go
+++ models/pedido_models/pedido.go
@@ -7,11 +7,13 @@ import (
 )
 
 type Pedido struct {
-	ID                primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	FechaPedido       time.Time          `bson:"fechaPedido" json:"fechaPedido,omitempty"`
-	FechaDevolucion   time.Time          `bson:"fechaDevolucion" json:"fechaDevolucion,omitempty"`
-	IdProyecto        primitive.ObjectID `bson:"idProyecto,omitempty" json:"idProyecto"`
-	InformePedido     string             `bson:"informePedido" json:"informePedido,omitempty"`
-	InformeDevolucion string             `bson:"informeDevolucion" json:"informeDevolucion,omitempty"`
-	Estado            bool               `bson:"estado" json:"estado,omitempty"`
+	ID                primitive.ObjectID   `bson:"_id,omitempty" json:"id"`
+	FechaPedido       time.Time            `bson:"fechaPedido" json:"fechaPedido,omitempty"`
+	FechaDevolucion   time.Time            `bson:"fechaDevolucion" json:"fechaDevolucion,omitempty"`
+	IdProyecto        primitive.ObjectID   `bson:"idProyecto,omitempty" json:"idProyecto"`
+	InformePedido     string               `bson:"informePedido" json:"informePedido,omitempty"`
+	InformeDevolucion string               `bson:"informeDevolucion" json:"informeDevolucion,omitempty"`
+	Estado            bool                 `bson:"estado" json:"estado,omitempty"`
+	RecursosID        []primitive.ObjectID `bson:"recursoid,omitempty" json:"recursoid"`
+	UsuarioID		  primitive.ObjectID `bson:"usuarioid,omitempty" json:"usuarioid"`
 }
