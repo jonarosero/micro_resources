9eccb00b6876e7322b78e7909e965ac96417286c
jonarosero <jars9605@hotmail.com>
2021-11-23 16:29:44 -0500

    Fix: list all recursos low information

--- bd/recurso_bd/listoRecurso.go
+++ bd/recurso_bd/listoRecurso.go
@@ -9,14 +9,12 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func ListoRecursos() ([]*recursomodels.DevuelvoRecurso, error) {
+func ListoRecursos() ([]*recursomodels.Recurso, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
 	defer cancel()
 
 	db := bd.MongoCN.Database("Recursos")
 	col := db.Collection("recurso")
-
-	var results []*recursomodels.DevuelvoRecurso
 
 	var resultadoRecurso []*recursomodels.Recurso
 
@@ -24,44 +22,20 @@ func ListoRecursos() ([]*recursomodels.DevuelvoRecurso, error) {
 
 	cur, err := col.Find(ctx, query)
 	if err != nil {
-		return results, err
+		return resultadoRecurso, err
 	}
 
 	for cur.Next(ctx) {
 		var s recursomodels.Recurso
 		err := cur.Decode(&s)
 		if err != nil {
-			return results, err
+			return resultadoRecurso, err
 		}
 		resultadoRecurso = append(resultadoRecurso, &s)
 	}
 
-	for _, recurso := range resultadoRecurso {
-		colTipo := db.Collection("tipoRecurso")
-		var tipo recursomodels.TipoRecurso
-
-		errTipo := colTipo.FindOne(ctx, bson.M{"_id": recurso.TipoID}).Decode(&tipo)
-
-		if errTipo != nil {
-			return results, err
-		}
-
-		var aux recursomodels.DevuelvoRecurso
-
-		aux.ID = recurso.ID
-		aux.NombreRecurso = recurso.NombreRecurso
-		aux.Imagen = recurso.Imagen
-		aux.CantidadDisponible = recurso.CantidadDisponible
-		aux.CantidadExistente = recurso.CantidadExistente
-		aux.TipoRecurso.ID = tipo.ID
-		aux.TipoRecurso.NombreTipo = tipo.NombreTipo
-		aux.TipoRecurso.DescripcionTipo = tipo.DescripcionTipo
-
-		results = append(results, &aux)
-	}
-
 	if err != nil {
-		return results, err
+		return resultadoRecurso, err
 	}
-	return results, err
+	return resultadoRecurso, err
 }
