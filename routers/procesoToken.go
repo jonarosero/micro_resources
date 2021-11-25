dc2af219ac558e718c900efc60f567b20bb5d7a7
jonarosero <jars9605@hotmail.com>
2021-11-24 21:35:05 -0500

    Fix: update procesoToken

--- routers/procesoToken.go
+++ routers/procesoToken.go
@@ -1,17 +1,21 @@
 package routers
 
 import (
+	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"net/http"
 	"strings"
 
 	"github.com/ascendere/resources/models"
 	"github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var Email string
 var IDUsuario string
-var IDRol string
 var Nombre string
+var Tk string
 
 func ProcesoToken(tk string) (*models.Claim, bool, string, error) {
 	miClave := []byte("MastersDelUniverso")
@@ -28,12 +32,45 @@ func ProcesoToken(tk string) (*models.Claim, bool, string, error) {
 		return miClave, nil
 	})
 
-	if len(claims.ID) > 0 {
-		Email = claims.Email
-		IDUsuario = claims.ID.Hex()
-		IDRol = claims.RolId
-		Nombre = claims.Nombre + " " + claims.Apellidos
+	if err == nil {
+		var usuario struct {
+			ID primitive.ObjectID `json:"id"`
+		}
 
+		client := &http.Client{}
+
+		req, errRequest := http.NewRequest("GET", "http://34.123.95.33/verPerfil?id="+claims.Id, nil)
+
+		if errRequest != nil {
+			return claims, false, "", errRequest
+		}
+
+		req.Header.Add("Accept", "application/json")
+		req.Header.Add("Content-Type", "application/json")
+		req.Header.Add("Authorization", "Bearer "+tk)
+
+		resp, error := client.Do(req)
+
+		if error != nil {
+			return claims, false, "", error
+		}
+
+		defer resp.Body.Close()
+
+		bodyBytes, errorBytes := ioutil.ReadAll(resp.Body)
+
+		if errorBytes != nil {
+			return claims, false, "", error
+		}
+
+		json.Unmarshal(bodyBytes, &usuario)
+
+		if len(usuario.ID) > 0 {
+			Email = claims.Email
+			IDUsuario = claims.ID.Hex()
+			Nombre = claims.Nombre + " " + claims.Apellidos
+			Tk = tk
+		}
 		return claims, true, IDUsuario, nil
 	}
 
@@ -43,3 +80,4 @@ func ProcesoToken(tk string) (*models.Claim, bool, string, error) {
 
 	return claims, false, string(""), err
 }
+
